3f4e0f1668e50e82ad8297a500c90e0b3b7d4055
kaushik samanta <kaushiksamanta23@gmail.com>
2017-11-03 16:56:38 -0400

    changed pre processing steps

--- Pre_Processing.m
+++ Pre_Processing.m
@@ -4,8 +4,8 @@ gray_image = rgb2gray(im);
 %% appplied gamma correction
 gamma_corrected = imadjust(gray_image,[],[],0.4);
 %% Enhancing the contrast of an intensity image using histogram equalization.
-% histrogram_equilization = histeq(gamma_corrected);
+histrogram_equilization = histeq(gamma_corrected);
 %% Noise Removal By Adaptive Filtering
-noise_removal = wiener2(gamma_corrected,[5 5]);
+noise_removal = wiener2(histrogram_equilization ,[5 5]);
 %% Image reduction using Gaussian Pyramid
 image = impyramid(noise_removal, 'reduce');
